726c3fabae7a2207d5f64c5f9802a3ec93e1eb36
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2020-11-21 16:03:28 +0545

    third commit

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import {Card, CardImg} from 'react-bootstrap';
 
+
 import Data from './shared/data.json';
 
 
+
 function Services(){
+        function clickHandler() {
+          
+            return (
+              console.log("Clicked in Card")
+            );
+          }
+          
+         
+ 
+
 return(
     <div className="row">
     { Data.map(post => {
         return(
             
-            <div className="col-12 col-md-5 m-1">
-                    <Card>
+            <div key= {post.id}className="col-12 col-md-5 m-1">
+                    <Card onClick={clickHandler}>
                         <CardImg width="100%" src={post.image} alt={post.name} />
                         <Card.Body>
                             <Card.Title> {post.name} </Card.Title>
